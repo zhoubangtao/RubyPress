15b39f1a3bd6239c152fea8311c2de9f35bb7bed
timen <zhoubangtao@163.com>
2014-04-21 14:52:03 +0800

    init project and modify .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyPress::Application.config.secret_key_base = '75de205fa3de654c40068cb21f42aab9e3ca48960f0166238ba125510e6c380d8e2cc7d42952cda13b7e09ad3cd839760e8624a31f6a00925ca3fc14b9210692'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyPress::Application.config.session_store :cookie_store, key: '_RubyPress_session'
